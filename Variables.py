d35eee72c54ec6c53535689c5f4e5bd5b163ad36
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-08 12:37:31 +0800

    hobby and age

---
+++ Variables.py
@@ -0,0 +1,9 @@
+blue="John"
+print(blue)
+india=12
+print(india)
+name= input("what is your age")
+print("\n name")
+print("my age is ",name)
+hobby=input("what is your hobby")
+print("my hobby is ",hobby)
