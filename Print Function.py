8df663b5e21d9b866edf23c343567d245aab1427
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-08 12:19:26 +0800

    Print

---
+++ Print Function.py
@@ -0,0 +1,8 @@
+#Print a Sentence
+print("welcome to the world of programming")
+#Print A Number
+print(7)
+#Next line after print
+print("hello",7)
+#End argument or Print statment
+print("welcome to Python",end="$")
